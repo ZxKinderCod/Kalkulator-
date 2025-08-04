b0eac3d77d6ba77dba1b0314fe85d16202570b0e
Gigih Ridho Apriansyah <gigihridhoa20@gmail.com>
2025-08-04 17:26:56 +0700

    fixed

--- main/bagi.py
+++ main/bagi.py
@@ -2,4 +2,4 @@ def pembagian ():
       angka1 = float(input("Masukkan angka pertama: "))
       angka2 = float(input("Masukkan angka kedua: "))
       hasil = angka1 / angka2
-      print(f"Hasil bagi: {hasil}")
+      print(f"Hasil pembagian: {hasil}")
